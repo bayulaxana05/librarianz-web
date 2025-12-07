b6272670a7a55ddd0f2e34e79d41121cefa805b1
bayulaxana05 <bynd4spd@gmail.com>
2025-12-07 21:04:04 +0700

    feat: redirect home page to books and remove home menu item

--- src/components/Header/menuData.ts
+++ src/components/Header/menuData.ts
@@ -2,12 +2,6 @@ import { Menu } from "@/types/Menu";
 
 export const menuData: Menu[] = [
   {
-    id: 1,
-    title: "Home",
-    newTab: false,
-    path: "/",
-  },
-  {
     id: 2,
     title: "Books",
     newTab: false,
